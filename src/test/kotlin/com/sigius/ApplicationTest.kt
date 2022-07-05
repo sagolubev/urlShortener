731af8f8e394768c160722e5d34b8cb912bb59a4
sergey <sergey.golubev@live.ru>
2022-07-05 10:47:49 +0400

    Added basic tests

--- src/test/kotlin/com/sigius/ApplicationTest.kt
+++ src/test/kotlin/com/sigius/ApplicationTest.kt
@@ -4,17 +4,28 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import net.pwall.json.test.JSONExpect.Companion.expectJSON
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-//class OrderRouteTests {
-//    @Test
-//    fun testGetOrder() = testApplication {
-//        val response = client.get("/order/2020-04-06-01")
-//        assertEquals(
-//            """{"number":"2020-04-06-01","contents":[{"item":"Ham Sandwich","amount":2,"price":5.5},{"item":"Water","amount":1,"price":1.5},{"item":"Beer","amount":3,"price":2.3},{"item":"Cheesecake","amount":1,"price":3.75}]}""",
-//            response.bodyAsText()
-//        )
-//        assertEquals(HttpStatusCode.OK, response.status)
-//    }
-//}
+class RootPathTest {
+    @Test
+    fun rootPathTest() = testApplication {
+        val response = client.get("/")
+        assertEquals(HttpStatusCode.OK, response.status)
+        expectJSON(response.bodyAsText()) {
+            property("appName", "urlShortener")
+        }
+    }
+}
+
+class UrlsPathTest {
+    @Test
+    fun rootPathTest() = testApplication {
+        val response = client.get("/url")
+        assertEquals(HttpStatusCode.NoContent, response.status)
+        expectJSON(response.bodyAsText()) {
+            property("noContentMessage", "No urls found")
+        }
+    }
+}
